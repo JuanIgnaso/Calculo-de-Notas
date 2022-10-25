389378dae2a4ee572f242922deadb890cf5ed8a9
juanIgnacio_Navarrete <juanford55@gmail.com>
2022-10-25 17:26:35 +0200

    Creadas vista y controlador del Calculo Notas

---
+++ controllers/CalculoNotas.juan_navarrete.controller.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ * 
+ * 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 Codigo GitHub
+ */
+
+$data['titulo'] = "Calculo de Notas";
+$data["div_titulo"] = "Formulario";
+
+include 'views/templates/header.php';
+include 'views/CalculoNotas.juan-navarrete.view.php';
+include 'views/templates/footer.php';
+
